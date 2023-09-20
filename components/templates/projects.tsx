78aad7520b37f5d314b69ec87b247c0bd2b10c30
Gilad T <2517870+giladt@users.noreply.github.com>
2023-09-20 18:48:39 +0200

    fix: remove unneeded container fragment

--- components/templates/projects.tsx
+++ components/templates/projects.tsx
@@ -19,21 +19,16 @@ interface ProjectsProps {
 
 const ProjectsPage: FC<ProjectsProps> = ({ projects }: ProjectsProps) => {
   return (
-    <>
-      <PageSection.Container
-        label="projects"
-        className="bg-stone-200 text-black"
-      >
-        <PageSection.Title align="left">Projects</PageSection.Title>
-        <PageSection.Tiles>
-          {projects.map((project, index) => (
-            <Project key={uuidv4()} priority={index <= 4} {...project}>
-              {project.description}
-            </Project>
-          ))}
-        </PageSection.Tiles>
-      </PageSection.Container>
-    </>
+    <PageSection.Container label="projects" className="bg-stone-200 text-black">
+      <PageSection.Title align="left">Projects</PageSection.Title>
+      <PageSection.Tiles>
+        {projects.map((project, index) => (
+          <Project key={uuidv4()} priority={index <= 4} {...project}>
+            {project.description}
+          </Project>
+        ))}
+      </PageSection.Tiles>
+    </PageSection.Container>
   );
 };
 export default ProjectsPage;
