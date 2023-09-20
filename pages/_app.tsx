f0396af93d3224911cd54f18f1d0823a547a353d
Gilad T <2517870+giladt@users.noreply.github.com>
2023-09-20 21:25:00 +0200

    fix: fontawesome icons change size on rerender ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,10 @@ import Nav from "@/components/organisms/nav";
 
 import { WindowContextProvider } from "@/contexts/windowContext";
 import "@/styles/globals.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
   const currentUrl = usePathname();
