eb9e5496cf6e8cca534d444d3e69e8416293d97d
Gilad Tsabar <2517870+giladt@users.noreply.github.com>
2025-05-08 22:55:06 +0200

    fixed build issues with sentry

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -5,11 +5,14 @@
 import {init, replayIntegration,captureRouterTransitionStart} from "@sentry/nextjs";
 
 init({
-  dsn: "https://940dfaad2a8d6eb230bb63df569ca6b4@o4509261157367808.ingest.de.sentry.io/4509261386743888",
+  dsn: "https://6861167508dc2f5e44dd9ea231c46be9@o4509261157367808.ingest.de.sentry.io/4509266560286800",
 
   // Add optional integrations for additional features
   integrations: [
-    replayIntegration(),
+    replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
   ],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
@@ -18,7 +21,7 @@ init({
   // Define how likely Replay events are sampled.
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 1.0,
 
   // Define how likely Replay events are sampled when an error occurs.
   replaysOnErrorSampleRate: 1.0,
