12e7a3c7e7160b6433257ac3f507b3e2232dd6e7
Gilad Tsabar <2517870+giladt@users.noreply.github.com>
2026-01-29 08:17:11 +0100

    fix build error

--- components/organisms/page-transition.tsx
+++ components/organisms/page-transition.tsx
@@ -1,62 +1,51 @@
-import React, { forwardRef } from "react";
-import { motion, HTMLMotionProps } from "framer-motion";
+import React, { forwardRef } from 'react';
+import { motion, HTMLMotionProps } from 'framer-motion';
 
-type TypTransitionVariants =
-  | "vertical"
-  | "vertical-exit"
-  | "horizontal"
-  | "horizontal-exit";
+type TypTransitionVariants = 'vertical' | 'vertical-exit' | 'horizontal' | 'horizontal-exit';
 
 type PageTransitionProps = {
-  dir: TypTransitionVariants;
-  id: string;
-} & HTMLMotionProps<"div">;
+    dir: TypTransitionVariants;
+    id: string;
+} & HTMLMotionProps<'div'>;
 type PageTransitionRef = React.ForwardedRef<HTMLDivElement>;
 
-function PageTransition(
-  { id, dir }: PageTransitionProps,
-  ref: PageTransitionRef
-) {
-  const variants = {
-    vertical: {
-      start: { scaleY: 0 },
-      animate: { scaleY: 0 },
-      end: { scaleY: 1.05 },
-    },
-    "vertical-exit": {
-      start: { scaleY: 1.05 },
-      animate: { scaleY: 0 },
-      end: { scaleY: 0 },
-    },
-    horizontal: {
-      start: { scaleX: 0 },
-      animate: { scaleX: 0 },
-      end: { scaleX: 1.05 },
-    },
-    "horizontal-exit": {
-      start: { scaleX: 1.05 },
-      animate: { scaleX: 0 },
-      end: { scaleX: 0 },
-    },
-  };
+function PageTransition({ id, dir }: PageTransitionProps, ref: PageTransitionRef) {
+    const variants = {
+        vertical: {
+            start: { scaleY: 0 },
+            animate: { scaleY: 0 },
+            end: { scaleY: 1.05 },
+        },
+        'vertical-exit': {
+            start: { scaleY: 1.05 },
+            animate: { scaleY: 0 },
+            end: { scaleY: 0 },
+        },
+        horizontal: {
+            start: { scaleX: 0 },
+            animate: { scaleX: 0 },
+            end: { scaleX: 1.05 },
+        },
+        'horizontal-exit': {
+            start: { scaleX: 1.05 },
+            animate: { scaleX: 0 },
+            end: { scaleX: 0 },
+        },
+    };
 
-  const transition = { duration: 0.4, ease: "easeInOut" };
-  const origin =
-    dir === "vertical" || dir === "vertical-exit"
-      ? "origin-top"
-      : "origin-right";
+    const origin = dir === 'vertical' || dir === 'vertical-exit' ? 'origin-top' : 'origin-right';
 
-  return (
-    <motion.div
-      key={id}
-      ref={ref}
-      className={`absolute top-0 left-0 w-full h-screen bg-stone-500 ${origin}`}
-      initial={variants[dir].start}
-      animate={variants[dir].animate}
-      exit={variants[dir].end}
-      transition={transition}
-    />
-  );
+    return (
+        <motion.div
+            key={id}
+            ref={ref}
+            className={`absolute top-0 left-0 w-full h-screen bg-stone-500 ${origin}`}
+            initial={variants[dir].start}
+            animate={variants[dir].animate}
+            exit={variants[dir].end}
+            transition={{ duration: 0.4, ease: 'easeInOut' }}
+        />
+    );
 }
 
 export default forwardRef(PageTransition);
